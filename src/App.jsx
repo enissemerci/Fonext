3df6029e4af888cb2a0c6a7c630a0c0a6382776b
Enis <enissemerci2@gmail.com>
2024-05-22 14:00:32 +0300

    İstatistik sayfasından sidebar kaldırıldı

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,17 @@ import { SideBar } from "./components/SideBar";
 import { Routes, Route } from "react-router-dom"; // Routes ve Route doğru şekilde import ediliyor
 import { Selamla } from "./screens/Selamla";
 import { NewContent } from "./components/NewContent";
+import Statistics from "./components/Statistics";
 
 function App() {
   return (
     <>
       <Header />
-      <SideBar />
       <Routes>
-        <Route path="/yeniiçerik" element={<NewContent/>}/>
         <Route exact path="/:userId" element={<Selamla />} />
+        <Route exact path="/" element={ <SideBar />} />
+        <Route path="/yeniiçerik" element={<NewContent />} />
+        <Route path="/istatistik" element={<Statistics />} />
       </Routes>
     </>
   );
