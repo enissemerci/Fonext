f9cdda6cb2edd13963a3fefa280c14b1e76e2ef3
Enis <enissemerci2@gmail.com>
2024-05-31 22:50:26 +0300

    NewContent page düzenlendi

--- src/components/NewContent.jsx
+++ src/components/NewContent.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
-import { Form, Button, Col, Row } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Col, Row } from "react-bootstrap";
+import bg from "../assets/BGG.mp4";
+import { FaFacebook, FaTwitter, FaYoutube,FaInstagram } from "react-icons/fa";
 
 export const NewContent = () => {
   const [content, setContent] = useState({
-    description: '',
-    file: '',
-    platform: ''
+    description: "",
+    file: "",
+    platform: [],
   });
-  const [imagePreview, setImagePreview] = useState('');
+  const [imagePreview, setImagePreview] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setContent(prevState => ({
+    setContent((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -28,59 +30,109 @@ export const NewContent = () => {
     }
   };
 
+  const handlePlatformChange = (e) => {
+    const { value, checked } = e.target;
+    setContent((prevState) => {
+      const newPlatforms = checked
+        ? [...prevState.platform, value]
+        : prevState.platform.filter((platform) => platform !== value);
+      return {
+        ...prevState,
+        platform: newPlatforms,
+      };
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(content);
   };
 
   return (
-    <div className="d-flex align-items-center justify-content-center" style={{ height: "80vh" }}>
-      <Form className='p-4 bg-secondary' onSubmit={handleSubmit}>
-        <Form.Group className='py-2' as={Row} controlId="description">
-          <Form.Label column sm="2">Açıklama:</Form.Label>
+    <div
+      className="d-flex align-items-center justify-content-center position-relative"
+      style={{ height: "92vh" }}
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          objectFit: "cover",
+          zIndex: -1,
+        }}
+      >
+        <source src={bg} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <Form className="p-4 rounded-4" onSubmit={handleSubmit} style={{ zIndex: 1 ,backgroundColor:"#FFD0D0" }}>
+        <Form.Group className="py-2" as={Row} controlId="description">
+          <Form.Label column sm="2">
+            Açıklama:
+          </Form.Label>
           <Col sm="10">
-            <Form.Control type="text" name="description" value={content.description} onChange={handleChange} />
+            <Form.Control
+              type="text"
+              name="description"
+              value={content.description}
+              onChange={handleChange}
+            />
           </Col>
         </Form.Group>
 
-        <Form.Group className='py-2' as={Row} controlId="file">
-          <Form.Label column sm="2">Dosya:</Form.Label>
+        <Form.Group className="py-2" as={Row} controlId="file">
+          <Form.Label column sm="2">
+            Dosya:
+          </Form.Label>
           <Col sm="10">
             <Form.Control type="file" name="file" onChange={handleFileChange} />
           </Col>
         </Form.Group>
 
-        <Form.Group className='py-2' as={Row} controlId="platform">
-          <Form.Label column sm="2">Platform:</Form.Label>
+        <Form.Group className="py-2" as={Row} controlId="platform">
+          <Form.Label column sm="2">
+            Platform:
+          </Form.Label>
           <Col sm="10">
-            <Form.Check 
-              type="radio" 
-              name="platform" 
-              id="youtube" 
-              label="YouTube" 
-              value="youtube" 
-              onChange={handleChange}
+          <Form.Check
+              type="checkbox"
+              name="platform"
+              id="youtube"
+              value="youtube"
+              onChange={handlePlatformChange}
+              label={<><FaYoutube /> YouTube</>}
             />
-            <Form.Check 
-              type="radio" 
-              name="platform" 
-              id="instagram" 
-              label="Instagram" 
-              value="instagram" 
-              onChange={handleChange}
+            <Form.Check
+              type="checkbox"
+              name="platform"
+              id="instagram"
+              value="instagram"
+              onChange={handlePlatformChange}
+              label={<><FaInstagram /> Instagram</>}
             />
-            <Form.Check 
-              type="radio" 
-              name="platform" 
-              id="facebook" 
-              label="Facebook" 
-              value="facebook" 
-              onChange={handleChange}
+            <Form.Check
+              type="checkbox"
+              name="platform"
+              id="facebook"
+              value="facebook"
+              onChange={handlePlatformChange}
+              label={<><FaFacebook /> Facebook</>}
             />
           </Col>
         </Form.Group>
 
-        {imagePreview && <img src={imagePreview} alt="Fotoğraf" style={{ maxWidth: "100%", maxHeight: "300px" }} />}
+        {imagePreview && (
+          <img
+            src={imagePreview}
+            alt="Fotoğraf"
+            style={{ maxWidth: "100%", maxHeight: "300px" }}
+          />
+        )}
         {/* imagePreview state'i doluysa, önizleme gösterilir */}
 
         <Button variant="primary" type="submit">
